2e6904d7c73043cfce1e1dae9b1bd2e2b13144d3
fatemanazari <fatemanazari78@gmail.com>
2023-10-27 09:22:58 +0330

    Decorate classes

--- person.rb
+++ person.rb
@@ -1,25 +1,37 @@
-class Person
+require_relative 'nameable'
+require_relative 'decorator'
+
+class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
 
-  def initialize(age, name: 'Unknown', parent_permission: true)
-    @id = generate_id
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
+    @generate_id = generate_id
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
 
-  def can_use_services?
-    of_age? || @parent_permission
-  end
-
   private
+
+  def generate_id
+    timestamp = Time.now.to_i
+    object_id_hex = (object_id << 1).to_s(16) # Shift left to make it positive
+    "ID-#{timestamp}-#{object_id_hex}"
+  end
 
   def of_age?
     @age >= 18
   end
 
-  def generate_id
-    rand(100_00..999_99)
+  public
+
+  def can_use_services?
+    of_age? || @parent_permission
+  end
+
+  def correct_name
+    @name
   end
 end
