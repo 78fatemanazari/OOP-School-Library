758cf53e8492d82f2f1de4e8f301ff41f5b37071
fatemanazari <fatemanazari78@gmail.com>
2023-10-27 22:49:56 +0330

    Edit student class

--- student.rb
+++ student.rb
@@ -1,16 +1,16 @@
 require_relative 'person'
-require_relative 'classroom'
 
 class Student < Person
   attr_accessor :classroom
 
-  def initialize(age, name: 'Unknown', parent_permission: true)
-    super(age, name, parent_permission)
+  def initialize(classroom, age, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission: parent_permission)
+    @classroom = classroom
   end
 
-  def assign_to_classroom(classroom)
+  def add_classroom(classroom)
+    classroom.students.push(self) unless classroom.students.include?(self)
     @classroom = classroom
-    classroom.add_student(self)
   end
 
   def play_hooky
