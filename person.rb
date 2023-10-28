68adcb41e6ea2c84a630b3cec176941b7920341a
fatemanazari <fatemanazari78@gmail.com>
2023-10-28 09:19:21 +0330

    Create the id for each people

--- person.rb
+++ person.rb
@@ -8,7 +8,7 @@ class Person < Nameable
 
   def initialize(age, name = 'Unknown', parent_permission: true)
     super()
-    @generate_id = Random.rand(1..1000)
+    @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
